03a023f475dae8fdcbbf8d6ad82fe832ce4552b9
Kirill <kirill.marchenko@praguecollege.cz>
2023-03-09 23:54:39 +0100

    Added some more files

--- src/main/java/com/example/database/MaketController.java
+++ src/main/java/com/example/database/MaketController.java
@@ -1,12 +1,15 @@
 package com.example.database;
+
 import javafx.fxml.Initializable;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class RelationsController implements Initializable {
+public class MaketController implements Initializable {
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
     }
 }
+
