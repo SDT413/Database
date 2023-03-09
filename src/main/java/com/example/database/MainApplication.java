7aa2f4347f919c04c95adce6817d0b5f5e0e8111
Kirill <kirill.marchenko@praguecollege.cz>
2023-03-09 20:35:04 +0100

    Work_Start

--- src/main/java/com/example/database/MainApplication.java
+++ src/main/java/com/example/database/MainApplication.java
@@ -1,6 +1,8 @@
 package com.example.database;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -8,7 +10,10 @@ import java.io.IOException;
 public class MainApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-
+        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("MainWindow.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setScene(scene);
+        stage.show();
     }
 
     public static void main(String[] args) {
--- src/main/java/com/example/database/MainController.java
+++ src/main/java/com/example/database/MainController.java
@@ -1,14 +1,5 @@
 package com.example.database;
 
-import javafx.fxml.FXML;
-import javafx.scene.control.Label;
-
 public class MainController {
-    @FXML
-    private Label welcomeText;
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
-    }
 }
